0312e66bf7fd86ba6e76b739e6e32d31cb72dbda
Nikhil Nigam <73078542+nikhilnigam0406@users.noreply.github.com>
2024-04-04 22:45:47 +0530

    Include CSS
    media Query, pseudo code, styles, javascripts: basic, function, datatypes,

---
+++ index_Day03(b).js
@@ -0,0 +1,140 @@
+// javascript loosly coupled and dyanamic type
+
+
+// function eve(){
+
+// console.log("Hello");
+// // var
+// var message = "Nikhil";
+// console.log(message);
+// // 9.2e+18
+// console.log(course); // undefined
+// var course = "MCA";
+    
+// };
+// eve();
+
+// function eve(){
+//     for(var i = 0; i<5;i++){
+//         console.log('--',i);
+//     }
+//     console.log(i)
+// }
+// eve();
+
+// let
+
+// msg = "Kiet" //javascript by default
+// let msg = "Hello"
+// console.log(msg)
+
+// // const
+// const a = '4'
+// console.log(a)
+// // a = 54
+// // console.log(a) //Error
+// console.log(typeof(a))
+// let arr= ['a','e','i','o','u']
+// arr[0]= "abcd"
+// console.log(arr)
+
+// String concatenation
+// let arr =4.3
+// let num = 4
+// console.log(arr+num)
+
+// let str = '12'
+// let age = 50
+// console.log(Number(str)+age)
+
+// ParseInt
+// let st= '12'
+// let ag = parseInt(st)
+// let a = Number(st)
+// console.log(Number(a)+ag)
+
+// let abc = 121;
+// let bca = 213;
+// if(abc == bca){
+//     console.log("1.Yes")
+// }else{
+//     console.log("No")
+// }
+// let cba = abc
+// if(abc===cba){
+//     console.log("2.Yes")
+// }
+// else{
+//     console.log("N0")
+// }
+// Spread Operator
+
+// let cba = ...abc
+// if(abc===cba){
+//     console.log("2.Yes")
+// }
+// else{
+//     console.log("N0")
+// }
+
+// const str = 'khan';
+// const userName = `My name is`;
+// const ab = `${userName} ${str}`;
+// console.log(ab);
+
+// function print (str = "nothing is here"){
+//     console.log(str);
+// }
+// print('nikhil')
+// let name = "Nikhil";
+// console.log(print(name))
+
+// function declaration
+// function_Assignment
+//  const ab = function print (a){
+//     console.log(str);
+// } 
+// print("hello")
+// console.log("ab")
+
+//anorymus fun
+// let anbs = function () (){
+//     console.log(str);
+// }
+
+// Arrow function
+// let c = () =>{
+//     console.log();
+// }
+// Switch case
+ 
+// Object
+
+// const obj  = new Object ();
+// obj[0] = 'zero'
+// obj[1] = 'one'
+// console.log(obj)
+
+// const ob = {
+//     1:'one',
+//     'name': 'Nikhil',
+//     12:'twelve'
+// }
+// console.log(ob)
+
+
+// const details= {
+//     'na' : 'nikhil',
+//     'no' : '123456',
+//     'add' : 'kanpur'
+// }
+// console.log(details)
+
+
+const details= {
+    'na' : 'nikhil',
+    'no' : '123456',
+    'add' : 'kanpur'
+}
+const z = prompt('Wlecome in js')
+
